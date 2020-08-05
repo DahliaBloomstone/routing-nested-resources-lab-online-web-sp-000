be89b5b5e5a356cf48304f292806da99d05f61f8
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-05 18:07:51 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
 class Artist < ActiveRecord::Base
-  has_many :songs
-end
+  def display_artist(song)
+      song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+    end
+  end 
