ab2e90ec2456d1aafce2017a2f91455c21102a1f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-05 18:16:10 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
 
   #nested resource routes to show all songs for an artist
-  #/artists/1/songs 
-  # individual songs for that artist /artist/1/songs/1 
+  #/artists/1/songs
+  # individual songs for that artist /artist/1/songs/1
 #index and show actions only 
